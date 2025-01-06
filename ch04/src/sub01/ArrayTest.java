ab032a17620acb9dda760793a5c87fe20bcf109e
seongmin <seongmin1419@naver.com>
2025-01-06 10:59:23 +0900

    add4

--- ch04/src/sub01/ArrayTest.java
+++ ch04/src/sub01/ArrayTest.java
@@ -7,7 +7,7 @@ package sub01;
 public class ArrayTest {
 	public static void main(String[] args) {
 		
-		// 배열 
+		// 배열 - 참조타입임.
 		int[]    arr1 = {1, 2, 3, 4, 5};
 		char[]   arr2 = {'A','B','C'};
 		String[] arr3 = {"서울", "대전", "대구", "부산", "광주"};
@@ -63,8 +63,34 @@ public class ArrayTest {
 		System.out.println("scores의 합계: " + total);
 		
 		
-		// 2차원 배열
+		// 2차원 배열  (행,열)
+		int[][] arr2d = {{1, 2, 3, 4}, 
+						 {5, 6, 7, 8}, 
+						 {9, 10, 11, 12}};
 		
-		// 3차원 배열
+		for(int a=0; a<3; a++) {
+			for(int b=0; b<4; b++) {
+				
+				System.out.println("arr2d["+a+"]["+b+"] :" + arr2d[a][b]);
+			}
+		}
+		
+		// 3차원 배열  (면,행,열)
+		
+		int[][][] arr3d = {{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, 
+						  {{10, 11, 12}, {13, 14, 15}, {16, 17, 18}},
+						  {{19, 20, 21}, {22, 23, 24}, {25, 26, 27}}};
+		
+		for(int a=0; a <3; a++) {
+			
+			for(int b=0; b<3; b++) {
+				
+				for(int c=0; c<3; c++) {
+					
+					System.out.printf("arr3d[%d][%d][%d] : %d\n", a, b, c, arr3d[a][b][c] );
+				}
+			}
+		}
+		
 	}
 }
