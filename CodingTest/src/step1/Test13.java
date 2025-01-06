fc3b3405cef9424fdf22e77b2fbee07e6ba00f18
seongmin <seongmin1419@naver.com>
2025-01-06 11:01:15 +0900

    add coding

---
+++ CodingTest/src/step1/Test13.java
@@ -0,0 +1,7 @@
+package step1;
+
+public class Test13 {
+	public static void main(String[] args) {
+		System.out.print("|\\_/| \n|q p|   /} \n( 0 )\"\"\"\\ \n|\"^\"`    | \n||_/=\\\\__|");
+	}
+}
---
+++ CodingTest/src/step2/Test5.java
@@ -0,0 +1,12 @@
+package step2;
+
+import java.util.Scanner;
+
+public class Test5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+	}
+}
