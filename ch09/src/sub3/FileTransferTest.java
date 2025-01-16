21e815ae62d2165485de340966ace2e2f9557cfe
seongmin <seongmin1419@naver.com>
2025-01-16 11:47:25 +0900

    add ch09

---
+++ ch09/src/sub3/FileTransferTest.java
@@ -0,0 +1,43 @@
+package sub3;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/*
+ * 날짜:2025/01/16
+ * 이름:김성민
+ * 내용:Java 파일 입출력 버퍼스트림 실습하기
+ */
+public class FileTransferTest {
+	public static void main(String[] args) {
+		//파일 경로 설정
+		String source = "C:\\Users\\lotte6\\Desktop\\sample.tif";  
+		String target = "C:\\Users\\lotte6\\Desktop\\sample2.tif";
+		
+		
+		try {
+			//스트림 생성
+			FileInputStream fis = new FileInputStream(source);    //입력 스트림
+			FileOutputStream fos = new FileOutputStream(target);  //출력 스트림
+			
+			//파일복사
+			fis.transferTo(fos);
+			
+			
+			//스트림 해제
+			fis.close();
+			fos.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("프로그램 종료...");
+	}
+}
